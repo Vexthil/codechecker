46eb1e7b4b65ca1ea18640cb2a17e8d61f26627b
David Crook <DGCrook@gmail.com>
2021-01-03 15:04:04 +0000

    Add batch files to run tools
    Add some batch files that allow the tools to be ran initially. These do have specific local file paths in and so these are generic at the moment but provide a useful initial example

---
+++ tools/plist_to_html/plist_to_html/run_html_generation.bat
@@ -0,0 +1,6 @@
+@echo off
+pushd
+
+D:\fone\branch\external\ego\dev\python3\bin\python.exe PlistToHtml.py -o D:\_my_stuff\repositories\processcdb\html D:\_my_stuff\repositories\processcdb\plist
+
+popd
---
+++ tools/report-converter/codechecker_report_converter/run_converter.bat
@@ -0,0 +1,6 @@
+@echo off
+pushd
+
+D:\fone\branch\external\ego\dev\python3\bin\python.exe cli.py -t clang-tidy -o D:\_my_stuff\repositories\processcdb\plist D:\_my_stuff\repositories\processcdb\error.log
+
+popd
